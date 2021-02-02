f4687625ca64dfc3f1d4ae913d01572bece9b577
vikrambenipal <vikrambenipal@gmail.com>
2021-02-02 15:48:29 -0500

    error messages show up when submitting an empty form

--- form.js
+++ form.js
@@ -3,18 +3,30 @@ const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const form = document.getElementById('form');
-const errorElement = document.getElementById('error');
+const fnError = document.getElementById('fn-error');
+const lnError = document.getElementById('ln-error');
+const emError = document.getElementById('em-error');
+const pwError = document.getElementById('pw-error');
 
 console.log(firstName.placeholder);
 
 // prevent page from submitting
 form.addEventListener('submit', (e) => {
-    let messages = [];
+
     if(firstName.value === '' || firstName.value == null){
-        messages.push('Name is required');
-    }
-    if(messages.length > 0){
         e.preventDefault();
-        errorElement.innerText = messages.join(', ');
+        fnError.innerText = "First Name cannot be empty";
+    }
+    if(lastName.value === '' || lastName.value == null){
+        e.preventDefault();
+        lnError.innerText = "Last Name cannot be empty";
+    }
+    if(email.value === '' || email.value == null){
+        e.preventDefault();
+        emError.innerText = "Looks like this is not an email";
+    }
+    if(password.value === '' || password.value == null){
+        e.preventDefault();
+        pwError.innerText = "Password cannot be empty";
     }
 })
